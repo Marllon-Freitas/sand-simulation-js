c73da2a5fde933be986b743ae19aa8249436474b
Marllon Freitas <71530690+Marllon-Freitas@users.noreply.github.com>
2024-01-30 14:04:31 -0300

    refactor: refactoring part of the code

--- js/main.js
+++ js/main.js
@@ -12,6 +12,8 @@ let brushSizeController = document.getElementById("brush-size");
 const GRAIN_SIZE = 5;
 let grid;
 let cols, rows;
+let brushSize = 1;
+let action = '';
 
 function mouseDrag(e, value, brushSize = 1) {
   e.preventDefault();
@@ -30,10 +32,12 @@ function mouseDrag(e, value, brushSize = 1) {
   let halfMatrix = Math.floor(matrix / 2);
   for (let i = -halfMatrix; i <= halfMatrix; i++) {
     for (let j = -halfMatrix; j <= halfMatrix; j++) {
-      let x = mouseColumn + i;
-      let y = mouseRow + j;
-      if (Math.sqrt(i * i + j * j) <= halfMatrix && x >= 0 && x < cols && y >= 0 && y < rows) {
-        grid[x][y] = value;
+      if (Math.random() < 0.5) {
+        let x = mouseColumn + i;
+        let y = mouseRow + j;
+        if (Math.sqrt(i * i + j * j) <= halfMatrix && x >= 0 && x < cols && y >= 0 && y < rows) {
+          grid[x][y] = value;
+        }
       }
     }
   }
@@ -65,23 +69,20 @@ function setup() {
 }
 
 function draw() {
+  const colorMap = {
+    0: "#000",
+    1: "#F6D7B0",
+    2: "#ffffff",
+    3: "#8B4513"
+  };
+
   for (let i = 0; i < cols; i++) {
     for (let j = 0; j < cols; j++) {
-      let x = i * GRAIN_SIZE;
-      let y = j * GRAIN_SIZE;
-      if (grid[i][j] == 0) {
-        ctx.fillStyle = "#000";
-      } else if (grid[i][j] == 1) {
-        ctx.fillStyle = "#F6D7B0";
-      } else if (grid[i][j] == 2) {
-        ctx.fillStyle = "#0000FF";
-      } else if (grid[i][j] == 3) {
-        ctx.fillStyle = "#8B4513";
-      }
-      ctx.fillRect(x, y, GRAIN_SIZE, GRAIN_SIZE);
+      ctx.fillStyle = colorMap[grid[i][j]];
+      ctx.fillRect(i * GRAIN_SIZE, j * GRAIN_SIZE, GRAIN_SIZE, GRAIN_SIZE);
     }
   }
-  let direction = 1;
+
   let nextGrid = createGrid(cols, rows);
   for (let i = 0; i < cols; i++) {
     for (let j = 0; j < cols; j++) {
@@ -124,62 +125,58 @@ function draw() {
         } else {
           nextGrid[i][j] = 3;
         }
-      }
+      } 
     }
   }
   grid = nextGrid;
 }
 
-function init() {
-  setup();
-  let action = '';
-
-  function startAction(e) {
-    if (sandController.checked) {
-      action = 'sand';
-    } else if (staticController.checked) {
-      action = 'static';
-    } else if (clearController.checked) {
-      action = 'clear';
-    } else if (dirtController.checked) {
-      action = 'dirt';
-    } else if (waterController.checked) {
-      action = 'water';
-    }
+function startAction(e) {
+  if (sandController.checked) {
+    action = 'sand';
+  } else if (staticController.checked) {
+    action = 'static';
+  } else if (clearController.checked) {
+    action = 'clear';
+  } else if (dirtController.checked) {
+    action = 'dirt';
   }
-
-  function moveAction(e) {
-    let brushSize = brushSizeController.value;
-    if (action === 'sand') {
-      mouseDrag(e, 1, brushSize);
-    } else if (action === 'static') {
-      mouseDrag(e, 2, brushSize);
-    } else if (action === 'clear') {
-      mouseDrag(e, 0, brushSize);
-    } else if (action === 'dirt') {
-      mouseDrag(e, 3, brushSize);
-    } else if (action === 'water') {
-      mouseDrag(e, 4, brushSize);
-    }
-  }
-
-  function endAction(e) {
-    action = '';
-  }
-
-  canvas.addEventListener("mousedown", startAction);
-  canvas.addEventListener("mousemove", moveAction);
-  canvas.addEventListener("mouseup", endAction);
-
-  canvas.addEventListener("touchstart", startAction);
-  canvas.addEventListener("touchmove", moveAction);
-  canvas.addEventListener("touchend", endAction);
-
-  setInterval(draw, 20);
 }
 
-init();
+function moveAction(e) {
+  brushSize = brushSizeController.value;
+  if (action === 'sand') {
+    mouseDrag(e, 1, brushSize);
+  } else if (action === 'static') {
+    mouseDrag(e, 2, brushSize);
+  } else if (action === 'clear') {
+    mouseDrag(e, 0, brushSize);
+  } else if (action === 'dirt') {
+    mouseDrag(e, 3, brushSize);
+  } else {
+    action = '';
+  }
+}
+
+function endAction(e) {
+  action = '';
+}
+
+canvas.addEventListener("mousedown", startAction);
+canvas.addEventListener("mousemove", moveAction);
+canvas.addEventListener("mouseup", endAction);
+
+canvas.addEventListener("touchstart", startAction);
+canvas.addEventListener("touchmove", moveAction);
+canvas.addEventListener("touchend", endAction);
 
 clearButton.addEventListener("click", function () {
   setup();
 });
+
+function init() {
+  setup();
+  setInterval(draw, 1000 / 60);
+}
+
+init();
