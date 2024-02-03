f67dec5c69128f4ebfc9e8f9083edd3066914392
Marllon Freitas <71530690+Marllon-Freitas@users.noreply.github.com>
2024-02-03 19:12:15 -0300

    feat: adding a draw line tool

--- js/main.js
+++ js/main.js
@@ -9,14 +9,23 @@ let dirtController = document.getElementById("dirt");
 let waterController = document.getElementById("water");
 let brushSizeController = document.getElementById("brush-size");
 
+let lineBrushController = document.getElementById("line-brush");
+
 const GRAIN_SIZE = 5;
 let grid;
 let cols, rows;
 let brushSize = 1;
 let action = '';
 
+let startingPoint = null;
+
 function mouseDrag(e, value, brushSize = 1) {
   e.preventDefault();
+  console.log('AAAAAAAA', action);
+  if (action === 'line') {
+    console.log('AAAAAAAA line');
+    return;
+  }
   let offsetX, offsetY;
   if (e.touches) {
     offsetX = e.touches[0].pageX - e.target.offsetLeft;
@@ -131,7 +140,26 @@ function draw() {
   grid = nextGrid;
 }
 
+function getMousePos(e) {
+  let offsetX, offsetY;
+  if (e.touches) {
+    offsetX = e.touches[0].pageX - e.target.offsetLeft;
+    offsetY = e.touches[0].pageY - e.target.offsetTop;
+  } else {
+    offsetX = e.offsetX;
+    offsetY = e.offsetY;
+  }
+  let mouseColumn = Math.floor(offsetX / GRAIN_SIZE);
+  let mouseRow = Math.floor(offsetY / GRAIN_SIZE);
+  return { x: mouseColumn, y: mouseRow };
+}
+
 function startAction(e) {
+  if (lineBrushController.checked) {
+    startingPoint = getMousePos(e);
+    action = 'line';
+    return;
+  }
   if (sandController.checked) {
     action = 'sand';
   } else if (staticController.checked) {
@@ -144,6 +172,13 @@ function startAction(e) {
 }
 
 function moveAction(e) {
+  if (action === 'line') {
+    if (startingPoint) {
+      let endPoint = getMousePos(e);
+      drawLine(startingPoint, endPoint, 0);
+    }
+    return;
+  }
   brushSize = brushSizeController.value;
   if (action === 'sand') {
     mouseDrag(e, 1, brushSize);
@@ -154,12 +189,46 @@ function moveAction(e) {
   } else if (action === 'dirt') {
     mouseDrag(e, 3, brushSize);
   } else {
-    action = '';
+    return;
   }
 }
 
 function endAction(e) {
+  if (startingPoint) {
+    let endPoint = getMousePos(e);
+    drawLine(startingPoint, endPoint, 2); // Desenha uma linha
+    startingPoint = null;
+  }
   action = '';
+}
+
+function drawLine(pointA, pointB, value) {
+  let dx = Math.abs(pointB.x - pointA.x);
+  let dy = Math.abs(pointB.y - pointA.y);
+  let sx = (pointA.x < pointB.x) ? 1 : -1;
+  let sy = (pointA.y < pointB.y) ? 1 : -1;
+  let err = dx - dy;
+
+  let x = pointA.x;
+  let y = pointA.y;
+  while (true) {
+    if (x >= 0 && x < cols && y >= 0 && y < rows) {
+      if (grid[x][y] !== 2) 
+        grid[x][y] = value;
+    }
+    if (x === pointB.x && y === pointB.y) break;
+    let e2 = 2 * err;
+    if (e2 > -dy) {
+      err -= dy;
+      x += sx;
+    }
+    if (e2 < dx) {
+      err += dx;
+      y += sy;
+    }
+  }
+
+  return grid;
 }
 
 canvas.addEventListener("mousedown", startAction);
